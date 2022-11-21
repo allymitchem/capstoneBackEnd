49cbfe74bf50085f459557994e6c588e08e33afc
tons0trees <forrest@fmcclure.com>
2022-11-21 11:32:18 -0700

    client set up

--- db/index.js
+++ db/index.js
@@ -1,13 +1,3 @@
-const { query } = require("express");
-const { Client } = require("pg");
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/graceshopper-dev',
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-  });
-
-
-    
-    
 module.exports = {
+    ...require('./client')
 };
