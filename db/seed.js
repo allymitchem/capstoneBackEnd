f7315c0f20cd765ce88cdf43b0dce5786cf490c5
tons0trees <forrest@fmcclure.com>
2022-11-21 12:26:07 -0700

    maybe created the tables

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,14 @@
-const {client} = require('./index');
+const {client} = require('./client');
 
 async function dropTables() {
     try{
-
+        console.log("Dropping All Tables...")
+        await client.query(`
+            DROP TABLE IF EXISTS cart_item;
+            DROP TABLE IF EXISTS cart;
+            DROP TABLE IF EXISTS user;
+            DROP TABLE IF EXISTS item;
+        `)
     }catch (error) {
         throw error;
     }
@@ -11,6 +17,40 @@ async function dropTables() {
 async function createTables(){
     try{
        console.log("Starting to build tables...")
+
+       await client.query(`
+       CREATE TABLE item(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) UNIQUE NOT NULL,
+        author VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        genre VARCHAR(255) NOT NULL,
+        price INTEGER
+       )
+       ;`)
+
+       await client.query(`
+       CREATE TABLE user(
+        id SERIAL PRIMARY KEY,
+       )
+       ;`)
+
+       await client.query(`
+       CREATE TABLE cart(
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES user(id)
+       )
+       ;`)
+
+       await client.query(`
+       CREATE TABLE cart_item(
+        id SERIAL PRIMARY KEY,
+        "cartId" INTEGER REFERENCES cart(id)
+        "itemId" INTEGER REFERENCES item(id)
+        quantity INTEGER
+        UNIQUE ("cartId", "itemId")
+       )
+       ;`)
 
         console.log("Finished building tables!")
     }catch (error) {
@@ -23,6 +63,8 @@ async function createTables(){
 async function rebuildDB(){
     try{
         client.connect();
+        dropTables()
+        createTables()
         
 
     } catch(error){
