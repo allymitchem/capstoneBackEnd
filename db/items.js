dc086523255a95690d0963e12a71c59f3779834e
tons0trees <forrest@fmcclure.com>
2022-11-22 10:46:42 -0700

    timing issues

---
+++ db/items.js
@@ -0,0 +1,18 @@
+const client = require('./client');
+
+async function addBook({title, author,description,price,year}) {
+    try {
+        console.log('**in the function call',{title, author,description,price,year});
+        const {rows: book} = await client.query(`
+            INSERT INTO items(title, author, description, year, price)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *;
+        `,[title, author, description, year, price])
+        console.log('***after the insert',book);
+        return book
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {addBook};
