0d53da1bce106984a5abd27c18b2e8e417ec081f
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-28 09:37:42 -0700

    still troubleshooting register

--- api/users.js
+++ api/users.js
@@ -4,6 +4,11 @@ const jwt = require("jsonwebtoken")
 const { JWT_SECRET}= process.env
 const {getUserByUsername, getUser, createUser} = require("../db")
 
+usersRouter.use("",  (req, res, next) => {
+    console.log("request had been made to users")
+    next()
+})
+
 //need to create paths from users
 // users/login, /register, /id, /admin
 
@@ -34,45 +39,45 @@ usersRouter.get("/login", async  (req, res, next) => {
         next(error)
     }
     
-    usersRouter.post("/register", async (req, res, next) => {
+    usersRouter.post("", async (req, res, next) => {
+        console.log("i am working")
+    //     const { username, password, email } = req.body;
 
-        const { username, password, email } = req.body;
-
-        if (!username || !password || !email) {
-            next({
-                name: "MissingCredentialsError",
-                message: "Please supply username, password and email"
-            })
-        }
+    //     if (!username || !password || !email) {
+    //         next({
+    //             name: "MissingCredentialsError",
+    //             message: "Please supply username, password and email"
+    //         })
+    //     }
         
-        if (password.length < 8) {
-            next({
-              name: "PasswordLengthError",
-              message: "Password Too Short!",
-            });
-          }
-        try { 
-            const _user = await getUserByUsername(username)
-            console.log(_user, "_user")
-            if (_user){
-                next({
-                    name: "UserAlreadyExists",
-                    message: `User ${username} is already taken.`
-                })
-            } else {
-                const user = await createUser({username, password, email})
-                console.log(user, "user line 64")
-                const  token = jwt.sign({id:user.id, username}, JWT_SECRET, {expiresIn: "2w"})
-                res.send({message: "Thank you for signing up!", token, user})
-            }
+    //     if (password.length < 8) {
+    //         next({
+    //           name: "PasswordLengthError",
+    //           message: "Password Too Short!",
+    //         });
+    //       }
+    //     try { 
+    //         const _user = await getUserByUsername(username)
+    //         console.log(_user, "_user")
+    //         if (_user){
+    //             next({
+    //                 name: "UserAlreadyExists",
+    //                 message: `User ${username} is already taken.`
+    //             })
+    //         } else {
+    //             const user = await createUser({username, password, email})
+    //             console.log(user, "user line 64")
+    //             const  token = jwt.sign({id:user.id, username}, JWT_SECRET, {expiresIn: "2w"})
+    //             res.send({message: "Thank you for signing up!", token, user})
+    //         }
 
-        } catch(error){
-            console.log(error)
-            next(error)
-        }
+    //     } catch(error){
+    //         console.log(error)
+    //         next(error)
+    //     }
 
     })
-})
+}) 
 
 
 
