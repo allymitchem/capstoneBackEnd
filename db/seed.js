2c9e73046a24a701be0a1f0b9aa0996a46e7d3a0
tons0trees <forrest@fmcclure.com>
2022-11-28 19:57:34 -0700

    some quality of life changes in the seed

--- db/seed.js
+++ db/seed.js
@@ -95,8 +95,7 @@ async function populateItems() {
         year: 1813,
         numInStock: 8,
         imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669606944/books/PrideAndPrejudice.jpg"
-    })
-    
+    })    
     await addBook({
         title: "1984", 
         author: "George Orwell", 
@@ -105,8 +104,7 @@ async function populateItems() {
         year: 1949,
         numInStock: 5,
         imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669606962/books/1984.jpg"
-    })
-    
+    })   
     await addBook({
         title: "To Kill A Mockingbird", 
         author: "Harper Lee", 
@@ -116,106 +114,105 @@ async function populateItems() {
         numInStock: 6,
         imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669606970/books/ToKillAMockingbird.jpg"
     })
-    
-    // await addBook({
-    //     title: "The Great Gatsby", 
-    //     author: "F. Scott Fitzgerald", 
-    //     description: "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted 'gin was the national drink and sex the national obsession,' it is an exquisitely crafted tale of America in the 1920s.", 
-    //     price: 1900, 
-    //     year: 1925,
-    //     numInStock: 9,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607223/books/TheGreatGatsby.jpg"
-    // })
-    // await addBook({
-    //     title: "The Catcher in the Rye", 
-    //     author: "J.D. Salinger", 
-    //     description: "The Catcher in the Rye is an all-time classic in coming-of-age literature- an elegy to teenage alienation, capturing the deeply human need for connection and the bewildering sense of loss as we leave childhood behind.", 
-    //     price: 2200, 
-    //     year: 1951,
-    //     numInStock: 10,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607210/books/theCatcherInTheRye.jpg"
-    // })
-    // await addBook({
-    //     title: "Catch-22", 
-    //     author: "Joseph Heller", 
-    //     description: "Set in Italy during World War II, this is the story of the incomparable, malingering bombardier, Yossarian, a hero who is furious because thousands of people he has never met are trying to kill him. But his real problem is not the enemy—it is his own army, which keeps increasing the number of missions the men must fly to complete their service. Yet if Yossarian makes any attempt to excuse himself from the perilous missions he’s assigned, he’ll be in violation of Catch-22, a hilariously sinister bureaucratic rule: a man is considered insane if he willingly continues to fly dangerous combat missions, but if he makes a formal request to be removed from duty, he is proven sane and therefore ineligible to be relieved.", 
-    //     price: 1500, 
-    //     year: 1961,
-    //     numInStock: 5,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607195/books/catch-22.jpg"
-    // })
-    // await addBook({
-    //     title: "Jane Eyre", 
-    //     author: "Charlotte Bronte", 
-    //     description: "Orphaned as a child, Jane has felt an outcast her whole young life. Her courage is tested once again when she arrives at Thornfield Hall, where she has been hired by the brooding, proud Edward Rochester to care for his ward Adèle. Jane finds herself drawn to his troubled yet kind spirit. She falls in love. Hard.", 
-    //     price: 2200, 
-    //     year: 1847,
-    //     numInStock: 12,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607090/books/JaneEyre.jpg"
-    // })
-    // await addBook({
-    //     title: "Great Expectations", 
-    //     author: "Charles Dickens", 
-    //     description: "In what may be Dickens's best novel, humble, orphaned Pip is apprenticed to the dirty work of the forge but dares to dream of becoming a gentleman — and one day, under sudden and enigmatic circumstances, he finds himself in possession of 'great expectations'.", 
-    //     price: 1500, 
-    //     year: 1860,
-    //     numInStock: 7,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607099/books/GreatExpectations.jpg"
-    // })
-    // await addBook({
-    //     title: "Wuthering Heights", 
-    //     author: "Emily Bronte", 
-    //     description: "At the centre of this novel is the passionate love between Catherine Earnshaw and Heathcliff - recounted with such emotional intensity that a plain tale of the Yorkshire moors acquires the depth and simplicity of ancient tragedy.", 
-    //     price: 2200, 
-    //     year: 1847,
-    //     numInStock: 12,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607111/books/WutheringHeights.jpg"
-    // })
-    // await addBook({
-    //     title: "Little Women", 
-    //     author: "Louisa May Alcott", 
-    //     description: "Here are talented tomboy and author-to-be Jo, tragically frail Beth, beautiful Meg, and romantic, spoiled Amy, united in their devotion to each other and their struggles to survive in New England during the Civil War.", 
-    //     price: 2200, 
-    //     year: 1868,
-    //     numInStock: 15,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607149/books/LittleWomen.jpg"
-    // })
-    // await addBook({
-    //     title: "Animal Farm", 
-    //     author: "George Orwell", 
-    //     description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality.", 
-    //     price: 1800, 
-    //     year: 1945,
-    //     numInStock: 25,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676222/books/AnimalFarm.jpg"
-    // })
-    // await addBook({
-    //     title: "Anna Karenina", 
-    //     author: "Leo Tolstoy", 
-    //     description: "Anna is a sophisticated woman who abandons her empty existence as the wife of Karenin and turns to Count Vronsky to fulfil her passionate nature - with tragic consequences.", 
-    //     price: 2100, 
-    //     year: 1878,
-    //     numInStock: 13,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676237/books/AnnaKarenina.jpg"
-    // })
-    // await addBook({
-    //     title: "Crime and Punishment", 
-    //     author: "Fyodor Dostoevsky", 
-    //     description: "Raskolnikov, a destitute and desperate former student, wanders through the slums of St Petersburg and commits a random murder without remorse or regret", 
-    //     price: 1500, 
-    //     year: 1866,
-    //     numInStock: 8,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676243/books/CrimeAndPunishment.jpg"
-    // })              
-    // await addBook({
-    //     title: "Lord of The Flies", 
-    //     author: "William Golding", 
-    //     description: "At the dawn of the next world war, a plane crashes on an uncharted island, stranding a group of schoolboys. At first, with no adult supervision, their freedom is something to celebrate; this far from civilization the boys can do anything they want. Anything. They attempt to forge their own society, failing, however, in the face of terror, sin and evil.", 
-    //     price: 2200, 
-    //     year: 1954,
-    //     numInStock: 13,
-    //     imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676251/books/LordOfTheFlies.jpg"
-    // })
+    await addBook({
+        title: "The Great Gatsby", 
+        author: "F. Scott Fitzgerald", 
+        description: "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted 'gin was the national drink and sex the national obsession,' it is an exquisitely crafted tale of America in the 1920s.", 
+        price: 1900, 
+        year: 1925,
+        numInStock: 9,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607223/books/TheGreatGatsby.jpg"
+    })
+    await addBook({
+        title: "The Catcher in the Rye", 
+        author: "J.D. Salinger", 
+        description: "The Catcher in the Rye is an all-time classic in coming-of-age literature- an elegy to teenage alienation, capturing the deeply human need for connection and the bewildering sense of loss as we leave childhood behind.", 
+        price: 2200, 
+        year: 1951,
+        numInStock: 10,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607210/books/theCatcherInTheRye.jpg"
+    })
+    await addBook({
+        title: "Catch-22", 
+        author: "Joseph Heller", 
+        description: "Set in Italy during World War II, this is the story of the incomparable, malingering bombardier, Yossarian, a hero who is furious because thousands of people he has never met are trying to kill him. But his real problem is not the enemy—it is his own army, which keeps increasing the number of missions the men must fly to complete their service. Yet if Yossarian makes any attempt to excuse himself from the perilous missions he’s assigned, he’ll be in violation of Catch-22, a hilariously sinister bureaucratic rule: a man is considered insane if he willingly continues to fly dangerous combat missions, but if he makes a formal request to be removed from duty, he is proven sane and therefore ineligible to be relieved.", 
+        price: 1500, 
+        year: 1961,
+        numInStock: 5,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607195/books/catch-22.jpg"
+    })
+    await addBook({
+        title: "Jane Eyre", 
+        author: "Charlotte Bronte", 
+        description: "Orphaned as a child, Jane has felt an outcast her whole young life. Her courage is tested once again when she arrives at Thornfield Hall, where she has been hired by the brooding, proud Edward Rochester to care for his ward Adèle. Jane finds herself drawn to his troubled yet kind spirit. She falls in love. Hard.", 
+        price: 2200, 
+        year: 1847,
+        numInStock: 12,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607090/books/JaneEyre.jpg"
+    })
+    await addBook({
+        title: "Great Expectations", 
+        author: "Charles Dickens", 
+        description: "In what may be Dickens's best novel, humble, orphaned Pip is apprenticed to the dirty work of the forge but dares to dream of becoming a gentleman — and one day, under sudden and enigmatic circumstances, he finds himself in possession of 'great expectations'.", 
+        price: 1500, 
+        year: 1860,
+        numInStock: 7,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607099/books/GreatExpectations.jpg"
+    })
+    await addBook({
+        title: "Wuthering Heights", 
+        author: "Emily Bronte", 
+        description: "At the centre of this novel is the passionate love between Catherine Earnshaw and Heathcliff - recounted with such emotional intensity that a plain tale of the Yorkshire moors acquires the depth and simplicity of ancient tragedy.", 
+        price: 2200, 
+        year: 1847,
+        numInStock: 12,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607111/books/WutheringHeights.jpg"
+    })
+    await addBook({
+        title: "Little Women", 
+        author: "Louisa May Alcott", 
+        description: "Here are talented tomboy and author-to-be Jo, tragically frail Beth, beautiful Meg, and romantic, spoiled Amy, united in their devotion to each other and their struggles to survive in New England during the Civil War.", 
+        price: 2200, 
+        year: 1868,
+        numInStock: 15,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669607149/books/LittleWomen.jpg"
+    })
+    await addBook({
+        title: "Animal Farm", 
+        author: "George Orwell", 
+        description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality.", 
+        price: 1800, 
+        year: 1945,
+        numInStock: 25,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676222/books/AnimalFarm.jpg"
+    })
+    await addBook({
+        title: "Anna Karenina", 
+        author: "Leo Tolstoy", 
+        description: "Anna is a sophisticated woman who abandons her empty existence as the wife of Karenin and turns to Count Vronsky to fulfil her passionate nature - with tragic consequences.", 
+        price: 2100, 
+        year: 1878,
+        numInStock: 13,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676237/books/AnnaKarenina.jpg"
+    })
+    await addBook({
+        title: "Crime and Punishment", 
+        author: "Fyodor Dostoevsky", 
+        description: "Raskolnikov, a destitute and desperate former student, wanders through the slums of St Petersburg and commits a random murder without remorse or regret", 
+        price: 1500, 
+        year: 1866,
+        numInStock: 8,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676243/books/CrimeAndPunishment.jpg"
+    })              
+    await addBook({
+        title: "Lord of The Flies", 
+        author: "William Golding", 
+        description: "At the dawn of the next world war, a plane crashes on an uncharted island, stranding a group of schoolboys. At first, with no adult supervision, their freedom is something to celebrate; this far from civilization the boys can do anything they want. Anything. They attempt to forge their own society, failing, however, in the face of terror, sin and evil.", 
+        price: 2200, 
+        year: 1954,
+        numInStock: 13,
+        imageURL: "https://res.cloudinary.com/fsa2/image/upload/v1669676251/books/LordOfTheFlies.jpg"
+    })
               
 }
 
@@ -227,7 +224,7 @@ async function populateCarts() {
     await createCart(2)
 
     await addBooktoCart({
-        itemId: 1, 
+        itemId: 5, 
         cartId: 1, 
         quantity: 1
     })
@@ -239,17 +236,17 @@ async function populateCarts() {
     })
 
     await addBooktoCart({
-        itemId: 2, 
+        itemId: 10, 
         cartId: 2, 
         quantity: 1
     })
     await addBooktoCart({
-        itemId: 2, 
+        itemId: 3, 
         cartId: 3, 
         quantity: 1
     })
     await addBooktoCart({
-        itemId: 2, 
+        itemId: 9, 
         cartId: 4, 
         quantity: 1
     })
@@ -313,8 +310,8 @@ async function testDB() {
         // const userByEmail = await getUserByEmail("johndoe@gmail.com")
         // console.log("user by email", userByEmail)
 
-        const allBooks = await getAllBooks()
-        console.log("all books:",allBooks);
+        // const allBooks = await getAllBooks()
+        // console.log("all books:",allBooks);
         
         // const someBooks = await getBooksByAuthor("dr. suess")
         // console.log("some books:",someBooks);
@@ -330,26 +327,26 @@ async function testDB() {
         // console.log(newBook3, "this is updated book 3")
         // console.log("finished testing database")
 
-        const cartlist = await getBooksInCart(1)
-        console.log("this is a list of the items:", cartlist);
+        // const cartlist = await getBooksInCart(1)
+        // console.log("this is a list of the items:", cartlist);
 
-        const cart1 = await getCart(1)
-        console.log("this is cart #1", cart1);
+        // const cart1 = await getCart(1)
+        // console.log("this is cart #1", cart1);
         
-        const cart1withbooks = await getCartWithBooks(1)
-        console.log("this is cart #1 with its books", cart1withbooks);
+        // const cart1withbooks = await getCartWithBooks(1)
+        // console.log("this is cart #1 with its books", cart1withbooks);
 
-        // const deletedCart = await deleteCart(1)
-        // console.log(deletedCart);
+        // // const deletedCart = await deleteCart(1)
+        // // console.log(deletedCart);
 
-        const updatedCart = await updateCart( {cartId: 1, active: false})
-        console.log("this is the updated cart", updatedCart)
+        // const updatedCart = await updateCart( {cartId: 1, active: false})
+        // console.log("this is the updated cart", updatedCart)
 
-        const updatedCartItem = await updateCartItem({cartItemId: 2, quantity: 6})
-        console.log("this is the updated quantity", updatedCartItem)
+        // const updatedCartItem = await updateCartItem({cartItemId: 2, quantity: 6})
+        // console.log("this is the updated quantity", updatedCartItem)
 
-        const activeCarts = await getActiveCarts()
-        console.log("this is all active cart ids", activeCarts)
+        // const activeCarts = await getActiveCarts()
+        // console.log("this is all active cart ids", activeCarts)
 
 
     } catch (error) {
