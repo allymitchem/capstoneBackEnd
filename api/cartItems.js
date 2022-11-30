1d8112ef5922fb2ec9a80963c136dff1d138f8fb
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-30 09:11:06 -0600

    cartitems functions that we can think of are all working

--- api/cartItems.js
+++ api/cartItems.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const { getCart, addBooktoCart, deleteCartItem, getCartWithBooks, getCartByUser, getActiveCartByUser } = require('../db')
+const { getCart, addBooktoCart, deleteCartItem, getCartWithBooks, getCartByUser, getActiveCartByUser, updateCartItem } = require('../db')
 const cartItemsRouter = express.Router()
 const {requireUser} = require("./utils")
 
 // ADD BOOK TO CART
 
-cartItemsRouter.post("/:cartId", async (req, res, next) => {
-    const  {itemId, cartId, quantity} = req.body
-    const id = req.params.cartId           
+cartItemsRouter.post("/:cartId", requireUser, async (req, res, next) => {
+    const  {itemId, quantity} = req.body
+    const {cartId} = req.params           
     
     try {
-        const cart = await getCart(id)
+        const cart = await getCart(cartId)
         if(cart.userId == req.user.id) {
             const newCartItem = await addBooktoCart({itemId, cartId, quantity})
             console.log(newCartItem, "this is the book that was added")
@@ -30,8 +30,8 @@ cartItemsRouter.post("/:cartId", async (req, res, next) => {
 
 // DELETE AN ITEM FROM CART
 
-cartItemsRouter.delete("/:cartItemId", async (req, res, next) => {
-    const cartItemId = req.params.cartItemId
+cartItemsRouter.delete("/:cartItemId", requireUser, async (req, res, next) => {
+    const {cartItemId} = req.params
     const userId = req.user.id
 
     try {
@@ -59,6 +59,38 @@ cartItemsRouter.delete("/:cartItemId", async (req, res, next) => {
         next({ error, name, message })
     }
 
+})
+
+// UPDATE CART BY THE USER
+
+cartItemsRouter.patch("/:cartItemId", requireUser, async (req, res, next) => {
+    const {cartItemId} = req.params
+    const userId = req.user.id
+    const {quantity} = req.body
+
+    try {
+        const cart = await getActiveCartByUser(userId)
+        if(cart.userId == req.user.id){
+            const cartItems = cart.items
+            const book = cartItems.filter(item => item.id == cartItemId)
+               if(book.length !== 0){
+                   const updatedCart = await updateCartItem({cartItemId, quantity})
+                   res.send(updatedCart)
+               } else {
+                    next({
+                        name: "Not Found",
+                        message: `There is not item with that ID in cart.`
+                     })
+                }
+        } else {
+            next({
+                name: "Unauthorized",
+                message: `You can't update books in this cart.`
+              })
+            }
+    } catch ({ error, name, message }) {
+        next({ error, name, message })
+    }
 })
 
 
